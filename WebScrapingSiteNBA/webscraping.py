a6f106b4d7698622299184034f631fd90072814a
Rodrigo Segui <46058568+Rodrigo-Segui@users.noreply.github.com>
2020-03-02 23:38:26 -0300

    commit 2

--- WebScrapingSiteNBA/webscraping.py
+++ WebScrapingSiteNBA/webscraping.py
@@ -5,6 +5,12 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import json
 
+#from selenium import webdriver
+#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+#binary = FirefoxBinary(‘C:\\Program Files\\Mozilla Firefox\\firefox.exe’)
+#driver = webdriver.Firefox(firefox_binary=binary, executable_path=r’C:\\geckodriver.exe’)
+
+
 #1 Pegar conteudo HTML a partir da URL
 url = "https://stats.nba.com/players/traditional/?PerMode=Totals&Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
 option = Options()
