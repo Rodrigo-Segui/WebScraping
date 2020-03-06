c3988fe2b6dccbf9e170a934586f9da8e547e4e3
Rodrigo Segui <46058568+Rodrigo-Segui@users.noreply.github.com>
2020-03-05 22:00:47 -0300

    update

--- WebScrapingSiteNBA/webscraping.py
+++ WebScrapingSiteNBA/webscraping.py
@@ -4,21 +4,20 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import json
+from selenium import webdriver
 
-#from selenium import webdriver
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-#binary = FirefoxBinary(‘C:\\Program Files\\Mozilla Firefox\\firefox.exe’)
-#driver = webdriver.Firefox(firefox_binary=binary, executable_path=r’C:\\geckodriver.exe’)
 
+
+browser = webdriver.Firefox()
 
 #1 Pegar conteudo HTML a partir da URL
-url = "https://stats.nba.com/players/traditional/?PerMode=Totals&Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
-option = Options()
-option.headless = True
-driver = webdriver.Firefox(options=option)
+#url = "https://stats.nba.com/players/traditional/?PerMode=Totals&Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
+#option = Options()
+#option.headless = True
+#driver = webdriver.Firefox(options=option)
 
-driver.get(url)
-driver.quit()
+#driver.get(url)
+#driver.quit()
 
 
 #2 Parsear a conteúdo HTML - BeaultifulSoup
