13eced2e9a483c5d336f5f6e2207f39599fb7d15
Rodrigo Segui <46058568+Rodrigo-Segui@users.noreply.github.com>
2020-03-07 13:33:51 -0300

    concluindo todas etapas

--- WebScrapingSiteNBA/webscraping.py
+++ WebScrapingSiteNBA/webscraping.py
@@ -9,30 +9,57 @@ import chromedriver_binary  # Adds chromedriver binary to path
 
 #1 Pegar conteudo HTML a partir da URL
 url = "https://stats.nba.com/players/traditional/?PerMode=Totals&Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
+top10ranking = {}
+
+rankings = {
+    '3points': {'field': 'FG3M', 'label': '3PM'},
+    'points': {'field': 'PTS', 'label': 'PTS'},
+    'assistants': {'field': 'AST', 'label': 'AST'},
+    'rebounds': {'field': 'REB', 'label': 'REB'},
+    'steals': {'field': 'STL', 'label': 'STL'},
+    'blocks': {'field': 'BLK', 'label': 'BLK'},
+}
+
+def buildrank(type):
+
+    field = rankings[type]['field']
+    label = rankings[type]['label']
+
+    driver.find_element_by_xpath(
+        f"//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='{field}']").click()
+
+    element = driver.find_element_by_xpath(
+        "//div[@class='nba-stat-table']//table")
+    html_content = element.get_attribute('outerHTML')
+
+    # Parse HTML (Parsear o conteúdo HTML) - BeaultifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.find(name='table')
+
+    # Data Structure Conversion (Estruturar conteúdo em um Data Frame) - Pandas
+    df_full = pd.read_html(str(table))[0].head(10)
+    df = df_full[['Unnamed: 0', 'PLAYER', 'TEAM', label]]
+    df.columns = ['pos', 'player', 'team', 'total']
+
+    # Convert to Dict (Transformar os Dados em um Dicionário de dados próprio)
+    return df.to_dict('records')
+
+
 option = Options()
 option.headless = True
 driver = webdriver.Chrome()
 driver.get(url)
-time.sleep(10)
+driver.implicitly_wait(10) #in seconds
 
-driver.find_element_by_xpath(
-    "//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='PTS']").click()
-element = driver.find_element_by_xpath("//div[@class='nba-stat-table']//table")
-html_content = element.get_attribute('outerHTML')
-print(html_content)
-
-#2 Parsear a conteúdo HTML - BeaultifulSoup
-#soup = BeautifulSoup(html_content, 'html.parser')
-#table = soup.find(name='table')
-
-#3 Estruturar conteúdo em um Data Frame de dados próprio
-#df_full = pd.read_html( str(table) )[0].head(10)
-#df = df_full [['Unnamed: 0', 'PLAYER', 'TEAM', 'PTS']]
-#df.columns = ['pos', 'player', 'team', 'total']
-#print(df)
-
-#4 Transformar os Dados em um Dicionário de dados próprio
-#5 Converter e salvar em um arquivo JSON
-
+for k in rankings:
+    top10ranking[k] = buildrank(k)
 
 driver.quit()
+#5 Converter e salvar em um arquivo JSON
+with open('ranking.json', 'w', encoding='utf-8') as jp:
+    js = json.dumps(top10ranking, indent=4)
+    jp.write(js)
+
+
+
+
